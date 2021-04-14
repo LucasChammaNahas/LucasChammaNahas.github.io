f0cb63e3138dffca0779e07b138c9302e9e515b1
lucasnahas <lucasahas@gmail.com>
2021-04-14 18:30:43 -0300

    site hc up

---
+++ header.php
@@ -0,0 +1,21 @@
+<div class="blue-bar"></div>
+    <header>
+      <logo><a href="./">HydroCavit</a></logo>
+      <nav>
+        <a href="./cana.php">Cana</a>
+        <a href="./milho.php">Milho</a>
+        <a href="./biogas.php">Biogás</a>
+        <a href="./cerveja.php">Cerveja</a>
+        <a href="./sucos.php">Sucos</a>
+        <a href="./laticinios.php">Laticínios</a>
+        <a href="./agua.php">Água</a>
+        <a href="./efluentes.php">Efluentes</a>
+        <a href="./oleos.php">Óleos</a>
+      </nav>
+      <menu>
+        <a onclick="_open()">
+          <img src="images/menu.svg" height="30">
+          <p>menu</p>
+        </a>
+      </menu>
+    </header>
---
+++ menu.php
@@ -0,0 +1,59 @@
+<div class="main-menu" id="main-menu-id">
+  <div class="close-shaded" onclick="_close()"></div>
+  <nav class="">
+
+    <a class="close-x" onclick="_close()">x</a>
+    <h4 class="">SOLUÇÕES PARA :</h4>
+    <a href="./cana.php" class="">
+      <img src="images/menu-cana.svg" alt="" class="">
+      <p class="">USINAS DE ETANOL DE CANA</p>
+    </a>
+    <a href="./milho.php" class="">
+      <img src="images/menu-milho.svg" alt="" class="">
+      <p class="">USINAS DE ETANOL DE MILHO</p>
+    </a>
+    <a href="./biogas.php" class="">
+      <img src="images/menu-biogas.svg" alt="" class="">
+      <p class="">GERAÇÃO DE BIOGÁS</p>
+    </a>
+    <a href="./cerveja.php" class="">
+      <img src="images/menu-cerveja.svg" alt="" class="">
+      <p class="">PRODUÇÃO DE CERVEJA</p>
+    </a>
+    <a href="./sucos.php" class="">
+      <img src="images/menu-sucos.svg" alt="" class="">
+      <p class="">PRODUÇÃO DE SUCOS</p>
+    </a>
+    <a href="./laticinios.php" class="">
+      <img src="images/menu-laticinios.svg" alt="" class="">
+      <p class="">PRODUÇÃO DE LATICÍNIOS</p>
+    </a>
+    <a href="./agua.php" class="">
+      <img src="images/menu-agua.svg" alt="" class="">
+      <p class="">TRATAMENTO DE ÁGUA</p>
+    </a>
+    <a href="./efluentes.php" class="">
+      <img src="images/menu-efluentes.svg" alt="" class="">
+      <p class="">TRATAMENTO DE EFLUENTES</p>
+    </a>
+    <a href="./oleos.php" class="last-item">
+      <img src="images/menu-oleos.svg" alt="" class="">
+      <p class="">REFINO DE ÓLEOS</p>
+    </a>
+
+    <h4 class="">SOBRE :</h4>
+    <a href="./tecnologia.php" class="">
+      <img src="images/menu-cavitador.svg" alt="" class="">
+      <p class="">A TECNOLOGIA</p>
+    </a>
+    <a href="./equipe.php" class="">
+      <img src="images/menu-equipe.svg" alt="" class="">
+      <p class="">A EQUIPE HYDROCAVIT</p>
+    </a>
+    <a href="./contato_page.php" class="last-item">
+      <img src="images/menu-contato.svg" alt="" class="">
+      <p class="">CONTATO</p>
+    </a>
+
+  </nav>
+</div>  
