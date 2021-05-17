d82d9faf6ff54cacc63f9cf127367f2d7169b190
lucasnahas <lucasahas@gmail.com>
2021-05-17 09:45:46 -0300

    implementacao da nova logica usando timeout e objeto global

--- src/script.js
+++ src/script.js
@@ -1,12 +1,18 @@
 
 // INITIALIZES THE CODE
+let $G = {
+  currentPage: 1,
+  resizing: false,
+  ctrlPressed: false,
+  scrollEnabled: true,
+};
 let currentPage = 1;
 let isScrollEnabled = true;
 document.getElementById('link1').click();
 
 
 // SETS EVENT LISTENERS TO THE HMTL
-window                                     .addEventListener('resize',  resizeScrollCancel);
+window                                     .addEventListener('resize',  resizeIssues);
 document.getElementsByTagName('html')[0]   .addEventListener('keydown', scrollOnZoomCancel1);
 document.getElementsByTagName('html')[0]   .addEventListener('keyup',   scrollOnZoomCancel2);
 document.getElementsByTagName('html')[0]   .addEventListener('wheel',   mousePageScroll);
@@ -14,26 +20,34 @@ document.getElementsByTagName('html')[0]   .addEventListener('keydown', keypadPa
 document.getElementById('proj-tic-tac-toe').addEventListener('click',   ticTacToeInit);
 
 
+// currentPage     -> $G.currentPage
+// isScrollEnabled -> $G.ctrlPressed
+// isScrollEnabled -> $G.scrollEnabled
+// .click()        -> location.href
+
+
 
 
 // PREVENTS PAGE FROM BUGGIN ON RESIZE OR ZOOM
-function resizeScrollCancel(){
-  document.getElementsByTagName('html')[0].style.scrollBehavior = 'auto';
+function resizeIssues(){
   document.getElementById(`link${ currentPage }`).click();
-  setTimeout(() => { 
-    document.getElementsByTagName('html')[0].style.scrollBehavior = 'smooth';
-  }, 100);
+
+  // document.getElementsByTagName('html')[0].style.scrollBehavior = 'auto';
+  // setTimeout(() => { 
+  //   document.getElementsByTagName('html')[0].style.scrollBehavior = 'smooth';
+  // }, 100);
 }
 
 
 // ZOOM CANCEL FUNCTIONS
-function scrollOnZoomCancel1(e){console.log('zoom cancel');
+function scrollOnZoomCancel1(e){
   let key = e.which || e.keyCode;
-  if(key == 17) { isScrollEnabled = false; }
+  if(key == 17) { isScrollEnabled = false; } //$G.ctrlPressed
+  x();
 }
 function scrollOnZoomCancel2(e){
   let key = e.which || e.keyCode;
-  if(key == 17) { isScrollEnabled = true; }
+  if(key == 17) { isScrollEnabled = true; } //$G.ctrlPressed
 }
 
 
@@ -83,10 +97,49 @@ function closeModal(){
   isScrollEnabled = true;
 }
 
-// let x = 0;
 
-// setTimeout(() => {
-//   console.log(x);
-// }, 1000);
 
-// x = 'pudim';
+
+
+
+
+let t;
+
+function x(){
+
+  /* 
+    INTERVAL
+      hard - smooth - hard
+      location.href
+
+    TRIGGERS
+      resize
+      mouse
+      keyboard
+  */
+
+
+  clearInterval(t);
+  t = setTimeout(() => {
+    console.log('intervalo');
+  }, 1000);
+}
+
+
+/* 
+  FUNCTION
+    mouse
+    resize
+
+  t1
+    tempo de ativacao: maior q escrolao e menor q tirar o dedo pra dois escrolls
+    le o objeto global e compara resize com soma de deltaY
+    se resize:true, return;
+    se resize:false, muda a pagina para o lado da soma de dY e ativa t2
+
+  t2
+    lida com a logica de html smooth
+    
+
+
+*/
